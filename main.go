86f51f17cfbf394d9f40d8bd6537fb89d0cffd71
Dovydas Valančius <dovydas.valancius@gmail.com>
2025-03-01 12:55:54 +0200

    Add backend views and remove frontend files. Initial setup for Go backend and clean up Svelte frontend

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"html/template"
+	"io"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+}
